44afe06fe9836a411e39cf74e6a9f5627d2a229c
kebabti0 <yusuf080206@hotmail.com>
2026-01-09 13:46:05 +0100

    test

--- vite.config.js
+++ vite.config.js
@@ -2,15 +2,14 @@ import { defineConfig } from 'vite'
 import { VitePWA } from 'vite-plugin-pwa'
 
 export default defineConfig({
-  base: './', // Wichtig für lokales Öffnen ohne Server
+  base: './', 
   plugins: [
     VitePWA({
       registerType: 'autoUpdate',
-      // includeAssets entfernen oder anpassen
       manifest: {
         name: 'RehaPlaner+',
         short_name: 'RehaPlaner',
-        start_url: './',
+        start_url: './index.html',
         display: 'standalone',
         background_color: '#F3F4F6',
         theme_color: '#2563EB',
@@ -29,7 +28,7 @@ export default defineConfig({
         ]
       },
       workbox: {
-        globPatterns: ['**/*.{html,js,css,png,json}'], // PNG und JSON hinzugefügt
+        globPatterns: ['**/*.{html,js,css,png,json}'],
         runtimeCaching: [
           {
             urlPattern: /^https:\/\/cdnjs\.cloudflare\.com/,
